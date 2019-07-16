237117db1b35a4735fa2f8a04674bafa78c6aee5
Facundo Adrian Lucianna <facundolucianna@gmail.com>
2019-07-16 16:41:27 -0300

    Agregado de metodos de abrir o cerrar cajas

--- main.py
+++ main.py
@@ -4,9 +4,28 @@ class Fila(object):
     """Clase base de fila"""
 
     def __init__(self):
-        """constructor de la clase Fila """
+        """constructor de la clase Fila"""
+        self.apertura = False
         self.enfila=0
         self.fila = []
+
+    def abrircaja(self):
+        """Metodo que abre una fila/caja"""
+        if self.apertura == False:
+            print("Abriendo caja")
+            self.apertura = True
+        else:
+            print("Caja ya abierta")
+
+    def cerrarcaja(self):
+        """Metodo que cierra una fila/caja"""
+        if self.apertura == True:
+            print("Cerrando caja")
+            self.enfila = 0
+            self.fila = []
+            self.apertura = False
+        else:
+            print("Caja ya cerrada")
 
 class FilaPreferencial(Fila):
     """Clase de la fila de los clientes preferenciales"""
@@ -14,6 +33,7 @@ class FilaPreferencial(Fila):
     def insertar(self, cliente):
         """Inserta un nuevo cliente en la fila preferencial"""
         if (cliente.categoria == "Preferencial"):
+            print("Cliente agregado a la fila preferencial")
             self.enfila+=1
             self.fila.append(cliente)
         else:
@@ -26,11 +46,19 @@ class FilaPreferencial(Fila):
 
     def abrircajanueva(self,maxenfila,filanueva):
         """Si maxenfila es menor que la cantidad de clientes actualmente en espera, abro nueva caja"""
-        #if (maxenfila < self.enfila):
-        #    filanueva =
-        pass
-
-
+        if (maxenfila < self.enfila):
+            if (filanueva.apertura == False) : #Chequeamos que la caja filanueva ya esta abierta o no, si no lo esta, la abre
+                print("Abriendo caja nueva")
+                filanueva.abrircaja()
+                mitadFila = self.enfila // 2 #Dividimos en dos a la fila
+                filanueva.enfila = self.enfila - mitadFila
+                self.enfila = mitadFila
+                filanueva.fila = self.fila[0:filanueva.enfila]  #Y guardamos los clientes en cada fila en mitades
+                self.fila = self.fila[filanueva.enfila:]
+            else:
+                print("Caja ya abierta, abra otra caja o si no hay mas cajas, no se puede hacer nada") #Si la caja esta abierta, avisa que la caja esta abierta
+        else:
+            print("No hay suficiente clientes como para abrir una nueva fila")
 
 class FilaGeneral(Fila):
     """Clase que mantiene una fila de clientes no preferenciales"""
@@ -38,6 +66,7 @@ class FilaGeneral(Fila):
     def insertar(self, cliente):
         """Inserta un nuevo cliente en la fila no preferencial"""
         if (cliente.categoria != "Preferencial"):
+            print("Cliente agregado a la fila general")
             self.enfila+=1
             self.fila.append(cliente)
         else:
@@ -63,4 +92,6 @@ class cliente(object):
 
 if __name__ == "__main__":
     """ simular una fila en una entidad bancaria"""
+    print("hola")
+
     pass
